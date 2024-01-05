6e97ae27ce03e684e07b48f5ef1ec77df346b1d3
inodentry <inodentry@users.noreply.github.com>
2024-01-05 18:07:33 +0000

    deploy: TheSeekerGame/TheSeeker@25fec22665d994db83008faa0c42b9a7fc64d4ac

--- api/implementors/std/os/fd/owned/trait.AsFd.js
+++
@@ -1,3 +0,0 @@
-(function() {var implementors = {
-"inotify":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.74.0/std/os/fd/owned/trait.AsFd.html\" title=\"trait std::os::fd::owned::AsFd\">AsFd</a> for <a class=\"struct\" href=\"inotify/struct.Inotify.html\" title=\"struct inotify::Inotify\">Inotify</a>"]]
-};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
---
+++ api/type.impl/std/primitive.i32.js
@@ -0,0 +1,9 @@
+(function() {var type_impls = {
+"alsa_sys":[],
+"khronos_egl":[],
+"serde":[],
+"wayland_client":[],
+"wayland_sys":[],
+"winit":[],
+"x11_dl":[]
+};if (window.register_type_impls) {window.register_type_impls(type_impls);} else {window.pending_type_impls = type_impls;}})()
