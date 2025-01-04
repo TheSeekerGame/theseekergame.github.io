51eb12bfbfb40feefdee05a1694c966d6af3b273
odecay <odecay@users.noreply.github.com>
2025-01-04 08:16:54 +0000

    deploy: TheSeekerGame/TheSeeker@ea2e8e79adea4031e395adf26f9b5a4690abf607

--- api/trait.impl/syn/parse/trait.Parse.js
+++ api/trait.impl/syn/parse/trait.Parse.js
@@ -1,5 +1,5 @@
 (function() {
-    var implementors = Object.fromEntries([["darling_core",[["impl <a class=\"trait\" href=\"https://docs.rs/syn/2.0.93/syn/parse/trait.Parse.html\" title=\"trait syn::parse::Parse\">Parse</a> for <a class=\"enum\" href=\"darling_core/ast/enum.NestedMeta.html\" title=\"enum darling_core::ast::NestedMeta\">NestedMeta</a>"]]]]);
+    var implementors = Object.fromEntries([["darling_core",[["impl <a class=\"trait\" href=\"https://docs.rs/syn/2.0.94/syn/parse/trait.Parse.html\" title=\"trait syn::parse::Parse\">Parse</a> for <a class=\"enum\" href=\"darling_core/ast/enum.NestedMeta.html\" title=\"enum darling_core::ast::NestedMeta\">NestedMeta</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
