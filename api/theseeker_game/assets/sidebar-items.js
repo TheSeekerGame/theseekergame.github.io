06bd22e4f8f324928f5638aab4ba8502b5ed006b
odecay <odecay@users.noreply.github.com>
2025-02-08 03:20:56 +0000

    deploy: TheSeekerGame/TheSeeker@f5eb1a78c89c9f7efd7ce84906c640006165686a

--- api/theseeker_game/assets/sidebar-items.js
+++ api/theseeker_game/assets/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"struct":["AssetsPlugin","MainMenuAssets","UiAssets"]};
+window.SIDEBAR_ITEMS = {"struct":["AssetsPlugin","DialogAssets","MainMenuAssets","UiAssets"]};
--- api/trait.impl/bevy_asset_loader/asset_collection/trait.AssetCollection.js
+++ api/trait.impl/bevy_asset_loader/asset_collection/trait.AssetCollection.js
@@ -1,9 +1,9 @@
 (function() {
-    var implementors = Object.fromEntries([["theseeker_game",[["impl <a class=\"trait\" href=\"theseeker_game/prelude/trait.AssetCollection.html\" title=\"trait theseeker_game::prelude::AssetCollection\">AssetCollection</a> for <a class=\"struct\" href=\"theseeker_game/assets/struct.MainMenuAssets.html\" title=\"struct theseeker_game::assets::MainMenuAssets\">MainMenuAssets</a>"],["impl <a class=\"trait\" href=\"theseeker_game/prelude/trait.AssetCollection.html\" title=\"trait theseeker_game::prelude::AssetCollection\">AssetCollection</a> for <a class=\"struct\" href=\"theseeker_game/assets/struct.UiAssets.html\" title=\"struct theseeker_game::assets::UiAssets\">UiAssets</a>"]]]]);
+    var implementors = Object.fromEntries([["theseeker_game",[["impl <a class=\"trait\" href=\"theseeker_game/prelude/trait.AssetCollection.html\" title=\"trait theseeker_game::prelude::AssetCollection\">AssetCollection</a> for <a class=\"struct\" href=\"theseeker_game/assets/struct.DialogAssets.html\" title=\"struct theseeker_game::assets::DialogAssets\">DialogAssets</a>"],["impl <a class=\"trait\" href=\"theseeker_game/prelude/trait.AssetCollection.html\" title=\"trait theseeker_game::prelude::AssetCollection\">AssetCollection</a> for <a class=\"struct\" href=\"theseeker_game/assets/struct.MainMenuAssets.html\" title=\"struct theseeker_game::assets::MainMenuAssets\">MainMenuAssets</a>"],["impl <a class=\"trait\" href=\"theseeker_game/prelude/trait.AssetCollection.html\" title=\"trait theseeker_game::prelude::AssetCollection\">AssetCollection</a> for <a class=\"struct\" href=\"theseeker_game/assets/struct.UiAssets.html\" title=\"struct theseeker_game::assets::UiAssets\">UiAssets</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":57,"fragment_lengths":[644]}
+//{"start":57,"fragment_lengths":[959]}
