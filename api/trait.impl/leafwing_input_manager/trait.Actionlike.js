f6e7aba10f683fa7a83ebdc725f0ac958f8f9c35
mnmaita <mnmaita@users.noreply.github.com>
2025-02-24 13:09:04 +0000

    deploy: TheSeekerGame/TheSeeker@2129ba393bda454333b4a6de1ec902dc7481bed0

--- api/trait.impl/leafwing_input_manager/trait.Actionlike.js
+++ api/trait.impl/leafwing_input_manager/trait.Actionlike.js
@@ -1,9 +1,9 @@
 (function() {
-    var implementors = Object.fromEntries([["theseeker_game",[["impl <a class=\"trait\" href=\"leafwing_input_manager/trait.Actionlike.html\" title=\"trait leafwing_input_manager::Actionlike\">Actionlike</a> for <a class=\"enum\" href=\"theseeker_game/game/player/enum.PlayerAction.html\" title=\"enum theseeker_game::game::player::PlayerAction\">PlayerAction</a>"]]]]);
+    var implementors = Object.fromEntries([["theseeker_game",[["impl <a class=\"trait\" href=\"leafwing_input_manager/trait.Actionlike.html\" title=\"trait leafwing_input_manager::Actionlike\">Actionlike</a> for <a class=\"enum\" href=\"theseeker_game/game/player/player_action/enum.PlayerAction.html\" title=\"enum theseeker_game::game::player::player_action::PlayerAction\">PlayerAction</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":57,"fragment_lengths":[324]}
+//{"start":57,"fragment_lengths":[353]}
