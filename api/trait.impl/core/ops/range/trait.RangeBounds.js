8589f4a56f5f84c3cc06ec7100b9323c7a45926b
c12hz <c12hz@users.noreply.github.com>
2025-03-28 17:57:40 +0000

    deploy: TheSeekerGame/TheSeeker@29e347b331de2c3a2d1b5f3163439aff69f8a876

--- api/trait.impl/core/ops/range/trait.RangeBounds.js
+++ api/trait.impl/core/ops/range/trait.RangeBounds.js
@@ -1,5 +1,5 @@
 (function() {
-    var implementors = Object.fromEntries([["winnow",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.85.0/core/ops/range/trait.RangeBounds.html\" title=\"trait core::ops::range::RangeBounds\">RangeBounds</a>&lt;<a class=\"primitive\" href=\"https://doc.rust-lang.org/1.85.0/std/primitive.usize.html\">usize</a>&gt; for <a class=\"struct\" href=\"winnow/stream/struct.Range.html\" title=\"struct winnow::stream::Range\">Range</a>"]]]]);
+    var implementors = Object.fromEntries([["winnow",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.85.1/core/ops/range/trait.RangeBounds.html\" title=\"trait core::ops::range::RangeBounds\">RangeBounds</a>&lt;<a class=\"primitive\" href=\"https://doc.rust-lang.org/1.85.1/std/primitive.usize.html\">usize</a>&gt; for <a class=\"struct\" href=\"winnow/stream/struct.Range.html\" title=\"struct winnow::stream::Range\">Range</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
